2c63c5524ef12283388a11f615f9a29ce9cf8798
riyo3350g <yechouben@gamail.com>
2024-01-02 20:55:59 +0100

    Task 6 Try Fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
-  return Promise.allSettled([promise1, promise2]).then((values) => values);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      for (const result of results) {
+        if (result.status === 'rejected') {
+          result.value = `Error: ${result.reason.message}`;
+          delete result.reason;
+        }
+      }
+      return results;
+    });
+
 }
