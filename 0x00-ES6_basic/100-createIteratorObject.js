9e0bcd8159e1567ae75ef6f32e386af77ad8e796
riyo3350g <yechouben@gamail.com>
2024-01-02 16:03:33 +0100

    Add Task 13 ADV

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+  const employees = [];
+  for (const key of Object.keys(report.allEmployees)) {
+    employees.push(...report.allEmployees[key]);
+  }
+
+  return employees;
+}
