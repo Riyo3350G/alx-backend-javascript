f5e4b92c258ceb9b6c6d140c367415e947c1525a
riyo3350g <yechouben@gamail.com>
2024-02-26 18:19:13 +0100

    Add Task 1 Req Fix 0

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,12 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const readLine = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-
-readLine.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  readLine.close();
-});
-
-readLine.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
 });
