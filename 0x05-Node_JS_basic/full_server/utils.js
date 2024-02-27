65d7968fdbf34cf44c46e359c9ab39f98f8c0ac2
riyo3350g <yechouben@gamail.com>
2024-02-27 22:29:35 +0100

    Add Task 8 Req Fix 1

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,26 +1,42 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 
-async function readDatabase(path) {
+module.exports = function readDatabase(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8')
-      .then((db) => {
-        const students = db.split('\n').filter((line) => line.trim() !== '');
-        const fields = {};
-        students.slice(1).forEach((student) => {
-          const field = student.split(',')[3];
-          const name = student.split(',')[0];
-          if (fields[field]) {
-            fields[field].push(name);
-          } else {
-            fields[field] = [name];
-          }
-        });
-        resolve(fields);
-      })
-      .catch(() => {
-        reject(new Error());
-      });
-  });
-}
+    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+      if (err) return reject(Error('Cannot load the database'));
+      // split data and taking only list without header
+      const lines = data.split('\n').slice(1, -1);
+      // give the header of data
+      const header = data.split('\n').slice(0, 1)[0].split(',');
+      // find firstname and field index
+      const idxFn = header.findIndex((ele) => ele === 'firstname');
+      const idxFd = header.findIndex((ele) => ele === 'field');
+      // declarate two dictionaries for count each fields and store list of students
+      const fields = {};
+      const students = {};
+      // it will contain all data
+      const all = {};
 
-module.exports = readDatabase;
+      lines.forEach((line) => {
+        const list = line.split(',');
+        if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
+        fields[list[idxFd]] += 1;
+        if (!students[list[idxFd]]) students[list[idxFd]] = '';
+        students[list[idxFd]] += students[list[idxFd]]
+          ? `, ${list[idxFn]}`
+          : list[idxFn];
+      });
+      for (const key in fields) {
+        if (Object.hasOwnProperty.call(fields, key)) {
+          const number = fields[key];
+          all[key] = {
+            students: `List: ${students[key]}`,
+            number,
+          };
+        }
+      }
+
+      return resolve(all);
+    });
+  });
+};
