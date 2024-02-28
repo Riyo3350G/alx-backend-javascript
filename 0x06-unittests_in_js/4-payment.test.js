f7bcf881f784749224f7fca303de0d69c854c46d
riyo3350g <yechouben@gamail.com>
2024-02-28 20:39:33 +0100

    Add Task 4 Req Fix 2

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,25 +1,36 @@
-const mocha = require('mocha');
-const { expect, assert } = require('chai');
 const sinon = require('sinon');
-
+const { expect } = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
-const { spy } = require('sinon');
 
 describe('sendPaymentRequestToApi', () => {
+  let calculateNumberStub;
+  let consoleLogSpy;
+
+  // eslint-disable-next-line jest/no-hooks
+  beforeEach(() => {
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+    calculateNumberStub.returns(10); // Always return 10
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  // eslint-disable-next-line jest/no-hooks
+  afterEach(() => {
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+
   // eslint-disable-next-line jest/prefer-expect-assertions
-  it('should call the calculateNumber function', () => {
-    const stub = sinon.stub(Utils, 'calculateNumber');
-    stub.returns(10);
-    const spy = sinon.spy(console, 'log');
-    const req = sendPaymentRequestToApi(100, 20);
-    // eslint-disable-next-line jest/valid-expect
-    expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.equal(true);
-    // eslint-disable-next-line jest/valid-expect
-    expect(spy.calledOnceWithExactly('The total is: 10'));
-    // eslint-disable-next-line jest/valid-expect
-    expect(Utils.calculateNumber('SUM', 100, 20)).to.equal(req);
-    stub.restore();
-    spy.restore();
+  it('should calculate the total payment', () => {
+    // eslint-disable-next-line no-undef
+    sendPaymentRequestToApi(100, 20);
+    // eslint-disable-next-line no-unused-expressions, jest/valid-expect
+    expect(calculateNumberStub.calledOnce).to.be.true;
+    // eslint-disable-next-line no-unused-expressions, jest/valid-expect
+    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+    // eslint-disable-next-line no-unused-expressions, jest/valid-expect
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    // eslint-disable-next-line no-unused-expressions, jest/valid-expect
+    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
   });
 });
