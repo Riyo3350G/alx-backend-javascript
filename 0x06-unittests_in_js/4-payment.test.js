8b094b580368c1531cddc86d9d632beb84f5bb07
riyo3350g <yechouben@gamail.com>
2024-02-28 21:05:30 +0100

    Add Task 4 Req Fix 3

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,36 +1,16 @@
+const { describe, it } = require('mocha');
 const sinon = require('sinon');
-const { expect } = require('chai');
+const assert = require('assert');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  let calculateNumberStub;
-  let consoleLogSpy;
-
-  // eslint-disable-next-line jest/no-hooks
-  beforeEach(() => {
-    calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
-    calculateNumberStub.returns(10); // Always return 10
-    consoleLogSpy = sinon.spy(console, 'log');
-  });
-
-  // eslint-disable-next-line jest/no-hooks
-  afterEach(() => {
-    calculateNumberStub.restore();
-    consoleLogSpy.restore();
-  });
-
-  // eslint-disable-next-line jest/prefer-expect-assertions
-  it('should calculate the total payment', () => {
-    // eslint-disable-next-line no-undef
+  // eslint-disable-next-line jest/prefer-expect-assertions, jest/expect-expect
+  it('should call the calculateNumber function', () => {
+    const spy = sinon.spy(console, 'log');
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
     sendPaymentRequestToApi(100, 20);
-    // eslint-disable-next-line no-unused-expressions, jest/valid-expect
-    expect(calculateNumberStub.calledOnce).to.be.true;
-    // eslint-disable-next-line no-unused-expressions, jest/valid-expect
-    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
-    // eslint-disable-next-line no-unused-expressions, jest/valid-expect
-    expect(consoleLogSpy.calledOnce).to.be.true;
-    // eslint-disable-next-line no-unused-expressions, jest/valid-expect
-    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
+    assert(spy.withArgs('The total is: 10').calledOnce);
+    assert(stub.withArgs('SUM', 100, 20).calledOnce);
   });
 });
