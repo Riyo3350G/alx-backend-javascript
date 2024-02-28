279c41c82ebe875f69cbc9489709c863f2221305
riyo3350g <yechouben@gamail.com>
2024-02-29 00:23:09 +0100

    Add Task 4 Req Fix 7

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -12,11 +12,11 @@ describe('sendPaymentRequestToApi', () => {
     const calcStubNum = sinon.stub(Utils, 'calculateNumber').returns(10);
     sendPaymentRequestToApi(100, 20);
     // eslint-disable-next-line no-unused-expressions, jest/valid-expect, no-undef
-    expect(calcStubNum.calledWith('SUM', 100, 20)).to.be.true;
+    expect(calcStubNum.calledWithExactly('SUM', 100, 20)).to.be.true;
     // eslint-disable-next-line no-unused-expressions, no-undef, jest/valid-expect
     expect(calcStubNum.alwaysReturned(10)).to.be.true;
     // eslint-disable-next-line no-unused-expressions, jest/valid-expect
-    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+    expect(consoleSpy.calledWithExactly('The total is: 10')).to.be.true;
     // eslint-disable-next-line no-undef
     calcStubNum.restore();
     consoleSpy.restore();
