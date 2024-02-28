0273f667f93d896ab29dbc88947651d4d0bb0c0f
riyo3350g <yechouben@gamail.com>
2024-02-28 23:28:44 +0100

    Add Task 4 Req Fix 4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,16 +1,25 @@
-const {describe, it} = require("mocha");
-const sinon = require("sinon");
-const sendPaymentRequestToApi = require("./4-payment");
-const Utils = require("./utils");
-const assert = require("assert");
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
 
-describe("sendPaymentRequestToApi", function() {
-    it("check that Utils.calculateNumber is stubbed", function() {
-	const spy = sinon.spy(console, "log");
-	const stub = sinon.stub(Utils, "calculateNumber").returns(10);
-	sendPaymentRequestToApi(100, 20);
+describe('sendPaymentRequestToApi', () => {
+  // eslint-disable-next-line jest/prefer-expect-assertions
+  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
+    const bigBrother = sinon.spy(console);
+    const dummy = sinon.stub(Utils, 'calculateNumber');
 
-	assert(spy.withArgs("The total is: 10").calledOnce);
-	assert(stub.withArgs("SUM", 100, 20).calledOnce);
-    });
+    dummy.returns(10);
+    sendPaymentRequestToApi(100, 20);
+    // eslint-disable-next-line no-unused-expressions, jest/valid-expect
+    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
+    // eslint-disable-next-line jest/valid-expect
+    expect(dummy.callCount).to.be.equal(1);
+    // eslint-disable-next-line no-unused-expressions, jest/valid-expect
+    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
+    // eslint-disable-next-line jest/valid-expect
+    expect(bigBrother.log.callCount).to.be.equal(1);
+    dummy.restore();
+    bigBrother.log.restore();
+  });
 });
