50b8f37ceba872cd37a8669f9cd3a565042d28d6
riyo3350g <yechouben@gamail.com>
2024-02-28 21:09:56 +0100

    Add Task 4 Req Fix 4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,16 +1,16 @@
-const { describe, it } = require('mocha');
-const sinon = require('sinon');
-const assert = require('assert');
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./4-payment');
+const {describe, it} = require("mocha");
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./4-payment");
+const Utils = require("./utils");
+const assert = require("assert");
 
-describe('sendPaymentRequestToApi', () => {
-  // eslint-disable-next-line jest/prefer-expect-assertions, jest/expect-expect
-  it('should call the calculateNumber function', () => {
-    const spy = sinon.spy(console, 'log');
-    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
-    sendPaymentRequestToApi(100, 20);
-    assert(spy.withArgs('The total is: 10').calledOnce);
-    assert(stub.withArgs('SUM', 100, 20).calledOnce);
-  });
+describe("sendPaymentRequestToApi", function() {
+    it("check that Utils.calculateNumber is stubbed", function() {
+	const spy = sinon.spy(console, "log");
+	const stub = sinon.stub(Utils, "calculateNumber").returns(10);
+	sendPaymentRequestToApi(100, 20);
+
+	assert(spy.withArgs("The total is: 10").calledOnce);
+	assert(stub.withArgs("SUM", 100, 20).calledOnce);
+    });
 });
