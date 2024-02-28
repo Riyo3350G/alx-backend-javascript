212edadd632c502dde6000528836b72cd0bb2a7d
riyo3350g <yechouben@gamail.com>
2024-02-28 17:29:38 +0100

    Add Task 2 Req Fix 0

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const { assert } = require('chai');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
   // eslint-disable-next-line jest/prefer-expect-assertions, jest/expect-expect
