f3434bdedf897cde171c44e38bbf6f6b9a2ae478
riyo3350g <yechouben@gamail.com>
2024-02-29 16:35:33 +0100

    Add Task 10 Req

---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,48 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('aPI integration test', () => {
+  const URL = 'http://localhost:7865';
+
+  it('gET / returns correct response', () => new Promise((done) => {
+    request.get(`${URL}/`, (_err, res, body) => {
+      // eslint-disable-next-line jest/valid-expect
+      expect(res.statusCode).to.be.equal(200);
+      // eslint-disable-next-line jest/valid-expect
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  }));
+
+  it('gET /cart/11 returns correct response', () => new Promise((done) => {
+    request.get(`${URL}/cart/11`, (_err, res, body) => {
+      // eslint-disable-next-line jest/valid-expect
+      expect(res.statusCode).to.be.equal(200);
+      // eslint-disable-next-line jest/valid-expect
+      expect(body).to.be.equal('Payment methods for cart 11');
+      done();
+    });
+  }));
+
+  it('correct status code when :id is NOT a number (=> 404)?', () => new Promise((done) => {
+    // eslint-disable-next-line no-unused-vars
+    request.get(`${URL}/cart/twelve`, (_err, res, _body) => {
+      // eslint-disable-next-line jest/valid-expect
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  }));
+
+  it('pOST /login returns correct response', () => new Promise((done) => {
+    request.post({
+      url: `${URL}/login`,
+      form: { userName: 'Redwan' },
+    }, (_err, res, body) => {
+      // eslint-disable-next-line jest/valid-expect
+      expect(res.statusCode).to.be.equal(200);
+      // eslint-disable-next-line jest/valid-expect
+      expect(body).to.be.equal('Welcome Redwan');
+      done();
+    });
+  }));
+});
