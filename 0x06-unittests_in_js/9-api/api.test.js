24ac07fe5bfe5ca2f6adf11c22fa30eb5807bf4d
riyo3350g <yechouben@gamail.com>
2024-02-29 16:07:17 +0100

    Add Task 9 Req

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('aPI integration test', () => {
+  const URL = 'http://localhost:7865';
+
+  it('gET / returns correct response', () => new Promise((done) => {
+    request.get(`${URL}/`, (_err, res, body) => {
+      // eslint-disable-next-line jest/valid-expect
+      expect(res.statusCode).to.be.equal(200);
+      // eslint-disable-next-line jest/valid-expect
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  }));
+
+  it('gET /cart/11 returns correct response', () => new Promise((done) => {
+    request.get(`${URL}/cart/11`, (_err, res, body) => {
+      // eslint-disable-next-line jest/valid-expect
+      expect(res.statusCode).to.be.equal(200);
+      // eslint-disable-next-line jest/valid-expect
+      expect(body).to.be.equal('Payment methods for cart 11');
+      done();
+    });
+  }));
+});
