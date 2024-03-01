c8231e8bf93d0f2dfe5ffdf94063833d0a0d089b
riyo3350g <yechouben@gamail.com>
2024-03-01 01:06:58 +0100

    Add Task 4 Req Fix 8

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,24 +1,19 @@
-const { expect } = require('chai');
 const sinon = require('sinon');
-
 const Utils = require('./utils');
+const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  const consoleSpy = sinon.spy(console, 'log');
-  // eslint-disable-next-line jest/prefer-expect-assertions
-  it('should call the calculateNumber function', () => {
-    // eslint-disable-next-line no-unused-vars
-    const calcStubNum = sinon.stub(Utils, 'calculateNumber').returns(10);
-    sendPaymentRequestToApi(100, 20);
-    // eslint-disable-next-line no-unused-expressions, jest/valid-expect, no-undef
-    expect(calcStubNum.calledWithExactly('SUM', 100, 20)).to.be.true;
-    // eslint-disable-next-line no-unused-expressions, no-undef, jest/valid-expect
-    expect(calcStubNum.alwaysReturned(10)).to.be.true;
-    // eslint-disable-next-line no-unused-expressions, jest/valid-expect
-    expect(consoleSpy.calledWithExactly('The total is: 10')).to.be.true;
-    // eslint-disable-next-line no-undef
-    calcStubNum.restore();
-    consoleSpy.restore();
-  });
+    it('should spy on the add function', () => {
+        const stubtub = sinon.stub(Utils, 'calculateNumber')
+        stubtub.returns(10)
+        const JB = sinon.spy(console, 'log')
+
+        sendPaymentRequestToApi(100, 20)
+        sinon.assert.calledWith(stubtub, 'SUM', 100, 20)
+        sinon.assert.calledWith(JB, 'The total is: 10');
+        stubtub.restore()
+        JB.restore()
+    })
 });
+
